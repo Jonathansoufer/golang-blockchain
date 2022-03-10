94acbb84bd13190559afb8fa69cbca5fd08000e2
jonathansoufer <jonathanferreira23@gmail.com>
2022-03-10 14:45:11 +0000

    add wallet module plus refactoring

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -4,23 +4,15 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/gob"
+	"encoding/hex"
 	"fmt"
+	"log"
 )
 
 type Transaction struct {
 	ID      []byte
 	Inputs  []TxInput
 	Outputs []TxOutput
-}
-type TxOutput struct {
-	Value  int
-	PubKey string
-}
-
-type TxInput struct {
-	ID        []byte
-	Out       int
-	Signature string
 }
 
 func (tx *Transaction) SetID() {
@@ -52,9 +44,35 @@ func CoinbaseTx(to, data string) *Transaction {
 func (tx *Transaction) IsCoinbase() bool {
 	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
 }
-func (in *TxInput) CanUnlock(data string) bool {
-	return in.Signature == data
-}
-func (out *TxOutput) CanBeUnlocked(data string) bool {
-	return out.PubKey == data
+
+func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction {
+	var inputs []TxInput
+	var outputs []TxOutput
+
+	acc, validOutputs := chain.FindSpendableOutputs(from, amount)
+
+	if acc < amount {
+		log.Panic("Error: not enough funds")
+	}
+
+	for txid, outs := range validOutputs {
+		txID, err := hex.DecodeString(txid)
+		Handle(err)
+
+		for _, out := range outs {
+			input := TxInput{txID, out, from}
+			inputs = append(inputs, input)
+		}
+	}
+
+	outputs = append(outputs, TxOutput{amount, to})
+
+	if acc > amount {
+		outputs = append(outputs, TxOutput{acc - amount, from})
+	}
+
+	tx := Transaction{nil, inputs, outputs}
+	tx.SetID()
+
+	return &tx
 }
